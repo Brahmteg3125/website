6d6e9b155d62c38b611e65bc4061ec33a28ea2a6
Brahmteg3125 <96515897+Brahmteg3125@users.noreply.github.com>
2025-08-05 18:46:53 +0530

    Update ContactSection.tsx

--- web-developer-portfolio-template/app/components/ContactSection.tsx
+++ web-developer-portfolio-template/app/components/ContactSection.tsx
@@ -34,7 +34,7 @@ export const ContactSection = () => {
 						Get in Touch
 					</a>
 					<a
-						href="/Brahmteg_amazon_new_resume.pdf"
+						href="web-developer-portfolio-template/app/components/Brahmteg_amazon_new_resume.pdf"
 						target="_blank"
 						className="inline-flex items-center justify-center gap-2 px-6 py-3 bg-[#21262D] border border-gray-800 rounded-lg font-medium hover:bg-gray-900 transition-colors"
 					>
